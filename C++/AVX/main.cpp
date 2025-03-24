f4a405518baa655055258e66d530daf31001c26d
ESU <gamack228@gmail.com>
2025-03-24 21:17:40 +0300

    bin search

--- C++/AVX/main.cpp
+++ C++/AVX/main.cpp
@@ -1,28 +1,82 @@
 #include <immintrin.h>
 #include <iostream>
+#include <chrono>
+#include <functional>
+
+namespace utils
+{
+    template <typename DurationType, typename IterationCleanUpCallback, typename Func, typename... Args>
+    DurationType time_it(size_t n, IterationCleanUpCallback &&callback, Func &&func, Args &&...args)
+        requires std::invocable<Func, Args...> &&
+                 requires { { std::chrono::duration_cast<DurationType>(std::chrono::high_resolution_clock::now() - std::chrono::high_resolution_clock::now()) }; }
+    {
+        using clock_type = std::chrono::high_resolution_clock;
+
+        clock_type::duration duration{0};
+
+        for (size_t i = 0; i < n; i++)
+        {
+            const auto start = clock_type::now();
+            std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
+            const auto end = clock_type::now();
+            duration += end - start;
+
+            std::invoke(std::forward<IterationCleanUpCallback>(callback));
+        }
+
+        return std::chrono::duration_cast<DurationType>(duration / n);
+    }
+
+    template <typename DurationType, typename Func, typename... Args>
+    DurationType time_it(Func &&func, Args &&...args)
+        requires std::invocable<Func, Args...> &&
+                 requires { { std::chrono::duration_cast<DurationType>(std::chrono::high_resolution_clock::now() - std::chrono::high_resolution_clock::now()) }; }
+    {
+        return time_it<DurationType>(1, [] {}, std::forward<Func>(func), std::forward<Args>(args)...);
+    }
+}
 
 int main()
 {
     constexpr auto N = 1000000000;
-    float sumTotal = 0.0f;
-    __m256 sumVec = _mm256_setzero_ps(); 
 
-    __m256 A256 = _mm256_set1_ps(1.0f);
-
-    for (int i = 0; i < N; i += 8)
+    // **AVX-версия**
+    float sumAVX = 0.0f;
+    auto avx_func = [&sumAVX, N]()
     {
-        sumVec = _mm256_add_ps(A256, sumVec); 
-    }
+        sumAVX = 0.0f;
+        __m256 sumVec = _mm256_setzero_ps();
+        __m256 A256 = _mm256_set1_ps(1.0f);
+        for (int i = 0; i < N; i += 8)
+        {
+            sumVec = _mm256_add_ps(A256, sumVec);
+        }
+        alignas(32) float sumArray[8];
+        _mm256_store_ps(sumArray, sumVec);
+        for (int j = 0; j < 8; j++)
+        {
+            sumAVX += sumArray[j];
+        }
+    };
 
-    alignas(32) float sumArray[8];
-    _mm256_store_ps(sumArray, sumVec);
+    auto avx_time = utils::time_it<std::chrono::milliseconds>(avx_func);
 
-    for (int j = 0; j < 8; j++)
+    float sumLoop = 0.0f;
+    auto loop_func = [&sumLoop, N]()
     {
-        sumTotal += sumArray[j]; 
-    }
+        sumLoop = 0.0f;
+        for (int i = 0; i < N; i++)
+        {
+            sumLoop += 1.0f;
+        }
+    };
 
-    std::cout << "Total sum: " << sumTotal << std::endl;
+    auto loop_time = utils::time_it<std::chrono::milliseconds>(loop_func);
+
+    printf("AVX time: %lld ms\n", avx_time.count());
+    printf("AVX sum: %f\n", sumAVX);
+    printf("Loop time: %lld ms\n", loop_time.count());
+    printf("Loop sum: %f\n", sumLoop);
 
     return 0;
 }
